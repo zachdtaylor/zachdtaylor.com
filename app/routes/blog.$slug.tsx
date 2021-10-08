a49a6fbf8d2a40d2b6b46cac11dfb5f19f39dabb
Zach Taylor <zachtylr04@gmail.com>
2021-10-07 21:13:39 -0600

    Add a test post

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -1,5 +1,5 @@
+import { useParams } from "react-router";
 import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
-import { useRouteData } from "remix";
 
 export let meta: MetaFunction = () => {
   return {
@@ -12,10 +12,11 @@ export let links: LinksFunction = () => {
   return [];
 };
 
-// export let loader: LoaderFunction = async () => {};
+export let loader: LoaderFunction = () => {
+  return "hi";
+};
 
-export default function Index() {
-  let data = useRouteData();
-
-  return <div style={{ textAlign: "center", padding: 20 }}>Hi</div>;
+export default function BlogPost() {
+  const params = useParams();
+  return <div>{params.slug}</div>;
 }
