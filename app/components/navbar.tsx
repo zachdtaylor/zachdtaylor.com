ce313ac569886c18c7963a6e551a1fce34d578cc
Zach Taylor <zachtylr04@gmail.com>
2021-10-09 16:45:04 -0600

    Add some style to nav bar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -11,23 +11,25 @@ type NavBarItemProps = {
   children: React.ReactNode;
 };
 
-function NavBarItem({ to, children }: NavBarItemProps) {
+function NavBarLink({ to, children }: NavBarItemProps) {
   return (
-    <li className="px-4 py-2">
-      <Link to={to}>{children}</Link>
+    <li className="px-5 pt-3 pb-2">
+      <Link to={to} className="text-lg">
+        {children}
+      </Link>
     </li>
   );
 }
 
 export default function NavBar() {
   return (
-    <nav className="flex flex-row p-5">
-      <div className="px-4 py-2">
+    <nav className="flex flex-row p-10 px-16">
+      <div className="pr-10 py-2 text-2xl">
         <Link to="/">Zach Taylor</Link>
       </div>
       <ul className="flex flex-row">
         {LINKS.map((link) => (
-          <NavBarItem to={link.to}>{link.name}</NavBarItem>
+          <NavBarLink to={link.to}>{link.name}</NavBarLink>
         ))}
       </ul>
     </nav>
--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,4 +1,12 @@
+import type { MetaFunction } from "remix";
 import NavBar from "~/components/navbar";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Blog",
+    description: "Read Zach Taylor's blog.",
+  };
+};
 
 export default function Blog() {
   return (
